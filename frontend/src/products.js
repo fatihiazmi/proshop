0c7de8c37c672738390e9709626526e06d114677
fatihiazmi <fatihiazmi@gmail.com>
2023-10-12 15:22:30 +0800

    frontend part

---
+++ frontend/src/products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth tech lets you connect it with compatible devices wirelessly hi-quality AAC audio offers immersive listening",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    stock: 10,
+    rating: 4.5,
+    reviews: 12,
+  },
+];
+
+export default products;
